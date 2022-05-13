663f671a855240005098ec3eef14de6f7398949b
Your Name <felix.acevedo.tech@gmail.com>
2022-05-13 14:52:33 -0400

    added signup.js

--- public/js/login.js
+++ public/js/login.js
@@ -22,32 +22,5 @@ async function loginFormHandler(event) {
     }
   }
   
-  async function signupFormHandler(event) {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (username && password) {
-      const response = await fetch('/api/users', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-      // automatically redirects users to the dashboard after successful login
-      if (response.ok) {
-        console.log(response);
-        document.location.replace('/dashboard/');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  }
-  
   document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-  
-  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
   
