9b22804f40e502c675c63ade6320b0e62ee67da2
Your Name <felix.acevedo.tech@gmail.com>
2023-03-28 21:18:33 -0400

    blocking out previous models for now

--- models/index.js
+++ models/index.js
@@ -1,34 +1,34 @@
-const User = require('./User');
-const Blog = require('./Blog');
-const Comment = require('./Comment');
+// const User = require('./User');
+// const Blog = require('./Blog');
+// const Comment = require('./Comment');
 
-// create associations
-User.hasMany(Blog, {
-    foreignKey: 'user_id'
-});
+// // create associations
+// User.hasMany(Blog, {
+//     foreignKey: 'user_id'
+// });
 
-Blog.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: 'SET NULL'
-});
+// Blog.belongsTo(User, {
+//     foreignKey: 'user_id',
+//     onDelete: 'SET NULL'
+// });
 
-Comment.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: 'SET NULL'
-});
+// Comment.belongsTo(User, {
+//     foreignKey: 'user_id',
+//     onDelete: 'SET NULL'
+// });
 
-User.hasMany(Comment, {
-    foreignKey: 'user_id',
-    onDelete: 'SET NULL'
-})
+// User.hasMany(Comment, {
+//     foreignKey: 'user_id',
+//     onDelete: 'SET NULL'
+// })
 
-Comment.belongsTo(Blog, {
-    foreignKey: 'blog_id',
-    onDelete: 'SET NULL'
-});
+// Comment.belongsTo(Blog, {
+//     foreignKey: 'blog_id',
+//     onDelete: 'SET NULL'
+// });
 
-Blog.hasMany(Comment, {
-    foreignKey: 'blog_id'
-});
+// Blog.hasMany(Comment, {
+//     foreignKey: 'blog_id'
+// });
 
-module.exports = { User, Blog, Comment };
+// module.exports = { User, Blog, Comment };
